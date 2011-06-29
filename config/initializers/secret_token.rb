a702a3647787516634159d6e52d22b7ec9c7a1c8
Rosa <rosa@innku.com>
2011-06-29 16:11:00 -0500

    Basic member model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InnkuSass::Application.config.secret_token = 'e18228706ea699e82a6ea9b0ae6596328e0e81f79e6552f688d1db71d5100c7366c725644b7bfb65a05b7c7b9e2e3f90dd63edd497d9f4e0a6c7bce48dc85edc'
